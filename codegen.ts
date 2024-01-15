74cc435b1a5e3f871eb99bb0e019ee2d1305b71e
Brandon Ting <brandon1221t@gmail.com>
2024-01-15 10:13:40 +0800

    fix gql codegen config to find all related files

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: 'https://beta.pokeapi.co/graphql/v1beta',
-  documents: ['src/**/*.vue', 'pages/**/*.vue'],
+  documents: ['src/**/*.{ts,vue}', 'pages/**/*.{ts,vue}', 'composables/**/*.{ts,vue}', 'components/**/*.{ts,vue}'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     './gql/': {
