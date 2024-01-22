7c0e9bfc2615377eb0d155fbd7d374a4598f2351
klay <ads1029384756@gmail.com>
2024-01-22 19:23:37 +0800

    feat: delete demo data

--- pages/index.vue
+++ pages/index.vue
@@ -1,191 +1,25 @@
 <script setup lang="ts">
-const pm1 = usePokemonDataStore('pokemon1')
-const pm2 = usePokemonDataStore('pokemon2')
-async function getPokemon() {
-  const targetPokemon1 = await useFetchPokemon()
-  const targetPokemon2 = await useFetchPokemon()
-  if (targetPokemon1) {
-    const random = Math.random() * 8
-    // 拿名字
-    pm1.name = targetPokemon1.name
-    // 拿圖片網址
-    pm1.imgUrl = random < 1 && targetPokemon1.sprite.front_shiny ? targetPokemon1.sprite.front_shiny : targetPokemon1.sprite.front_default
-    // 拿種族值
-    const stats = targetPokemon1.stats
-    pm1.baseStat = stats
-  }
-
-  if (targetPokemon2) {
-    const random = Math.random() * 8
-    // 拿名字
-    pm2.name = targetPokemon2.name
-    // 拿圖片網址
-    pm2.imgUrl = random < 1 && targetPokemon2.sprite.front_shiny ? targetPokemon2.sprite.front_shiny : targetPokemon2.sprite.front_default
-    // 拿種族值
-    const stats = targetPokemon2.stats
-    pm2.baseStat = stats
-  }
-}
 </script>
 
 <template>
   <div>
-    <Toolbar />
+    <toolbar />
     <div class="mx-auto text-center px-16">
       <div class="text-h3 pt-8 mb-8">
         {{ $t("hello") }}
       </div>
-      <v-btn @click="getPokemon()">
-        GO
-      </v-btn>
     </div>
-  </div>
-  <div class="d-flex">
-    <div class="px-16 w-50 mx-auto">
-      <div class="text-h4 text-center pt-8 mb-8">
-        {{ `${$t('go')}${$t(`pokemon.${pm1.name}`)}` }}
-      </div>
-      <v-img
-        class="mx-auto"
-        :width="150"
-        aspect-ratio="1/1"
-        :src="pm1.imgUrl"
-        cover
-      />
-      <div>
-        <v-progress-linear
-          v-model="pm1.baseStat.hp"
-          color="red"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm1.baseStat.hp }}</strong>
-        </v-progress-linear>
+    <v-container class="d-flex justify-center">
+      <v-row>
+        <v-col class="mx-auto" cols="12" md="6">
+          <pokemon-card />
+        </v-col>
 
-        <v-progress-linear
-          v-model="pm1.baseStat.atk"
-          color="blue-grey"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm1.baseStat.atk }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm1.baseStat.def"
-          color="amber"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm1.baseStat.def }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm1.baseStat.spa"
-          color="teal"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm1.baseStat.spa }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm1.baseStat.spd"
-          color="brown"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm1.baseStat.spd }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm1.baseStat.spe"
-          color="deep-purple"
-          height="20"
-          max="200"
-        >
-          <strong>{{ pm1.baseStat.spe }}</strong>
-        </v-progress-linear>
-      </div>
-    </div>
-    <div class="px-16 w-50 mx-auto">
-      <div class="text-h4 text-center pt-8 mb-8">
-        {{ `${$t('go')}${$t(`pokemon.${pm2.name}`)}` }}
-      </div>
-      <v-img
-        class="mx-auto"
-        :width="150"
-        aspect-ratio="1/1"
-        :src="pm2.imgUrl"
-        cover
-      />
-
-      <div>
-        <v-progress-linear
-          v-model="pm2.baseStat.hp"
-          color="red"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm2.baseStat.hp }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm2.baseStat.atk"
-          color="blue-grey"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm2.baseStat.atk }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm2.baseStat.def"
-          color="amber"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm2.baseStat.def }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm2.baseStat.spa"
-          color="teal"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm2.baseStat.spa }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm2.baseStat.spd"
-          color="brown"
-          height="20"
-          max="200"
-          class="mb-2"
-        >
-          <strong>{{ pm2.baseStat.spd }}</strong>
-        </v-progress-linear>
-
-        <v-progress-linear
-          v-model="pm2.baseStat.spe"
-          color="deep-purple"
-          height="20"
-          max="200"
-        >
-          <strong>{{ pm2.baseStat.spe }}</strong>
-        </v-progress-linear>
-      </div>
-    </div>
+        <v-col class="mx-auto" cols="12" md="6">
+          <pokemon-card />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -9,12 +9,12 @@ const darkMode: ThemeDefinition = {
   dark: true,
   colors: {
     'background': '#121212',
-    'surface': '212121',
+    'surface': '#212121',
     'primary': '#59359A',
     'primary-darken-1': '#533194',
     'secondary': '#DC3545',
     'secondary-darken-1': '#C42F3D',
-    'warning': '#FF0000',
+    'warning': '#FF0000'
   }
 }
 
@@ -27,7 +27,7 @@ const lightMode: ThemeDefinition = {
     'primary-darken-1': '#533194',
     'secondary': '#DC3545',
     'secondary-darken-1': '#C42F3D',
-    'warning': '#FF0000',
+    'warning': '#FF0000'
   },
 }
 
@@ -37,16 +37,16 @@ export default defineNuxtPlugin((app) => {
       defaultTheme: 'darkMode',
       themes: {
         darkMode,
-        lightMode
+        lightMode,
       }
     },
     icons: {
-      defaultSet: 'mdi',
+      defaultSet: 'mdi'
     },
     locale: {
       locale: 'zhHant',
       fallback: 'en',
-      messages: { zhHant, en, ja },
+      messages: { zhHant, en, ja }
     }
   })
   app.vueApp.use(vuetify)
