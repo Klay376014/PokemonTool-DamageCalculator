61a66fa659bb1e45c8549be6392653b1dbf2449d
klay <ads1029384756@gmail.com>
2024-02-13 20:38:10 +0800

    feat: ui design

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,26 @@
 <template>
   <div>
     <toolbar />
-    <v-container>
-      <v-row class="pt-8">
-        <v-col cols="12" md="6" class="px-0 px-sm-2 px-md-3">
-          <div class="text-h4 mb-3">
-            <div class="d-flex align-center">
+    <v-container class="px-1">
+      <v-row class="pt-8" no-gutters justify="center">
+        <v-col cols="12" md="8" lg="6" class="px-0 px-sm-2 px-md-3 mb-4 mb-lg-0">
+          <div class="d-flex flex-column flex-shrink-0">
+            <div class="d-flex align-center text-h4 mb-3">
               <v-icon icon="mdi-sword-cross" class="text-h5 text-primary mr-3" />
               <p>Attacker</p>
             </div>
+            <pokemon-card />
           </div>
-          <pokemon-card />
         </v-col>
 
-        <v-col cols="12" md="6" class="px-0 px-sm-2 px-md-3">
-          <div class="text-h4 mb-3">
-            <div class="d-flex align-center">
+        <v-col cols="12" md="8" lg="6" class="px-0 px-sm-2 px-md-3">
+          <div class="d-flex flex-column">
+            <div class="d-flex align-center text-h4 mb-3">
               <v-icon icon="mdi-shield-half-full" class="text-h5 text-secondary mr-3" />
               <p>Defender</p>
             </div>
+            <pokemon-card />
           </div>
-          <pokemon-card />
         </v-col>
       </v-row>
     </v-container>
