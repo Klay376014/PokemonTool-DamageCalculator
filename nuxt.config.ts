bf9eb5d7fb1239708b9f59fb87cdafc90c121c5b
Brandon Ting <brandon1221t@gmail.com>
2024-02-22 13:37:41 +0800

    try tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,6 +34,25 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
+    // ====== For Tauri
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://tauri.app/2/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+      hmr: {
+        // Use websocket for mobile hot reloading
+        protocol: 'ws',
+        // Make sure it's available on the network
+        host: '0.0.0.0',
+        // Use a specific port for hmr
+        port: 5183,
+      },
+    }
   },
   typescript: {
     typeCheck: true,
@@ -50,4 +69,7 @@ export default defineNuxtConfig({
       }
     }
   },
+  // For Tauri
+  ssr: false,
+
 })
