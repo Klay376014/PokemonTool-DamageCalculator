7e6a7a1a3220b2fc75ac67195518cd58ce51d5e3
Brandon Ting <brandon1221t@gmail.com>
2024-03-07 10:55:54 +0800

    Revert "try tauri"
    This reverts commit bf9eb5d7fb1239708b9f59fb87cdafc90c121c5b.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,25 +34,6 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
-    // ====== For Tauri
-    // Better support for Tauri CLI output
-    clearScreen: false,
-    // Enable environment variables
-    // Additional environment variables can be found at
-    // https://tauri.app/2/reference/environment-variables/
-    envPrefix: ['VITE_', 'TAURI_'],
-    server: {
-      // Tauri requires a consistent port
-      strictPort: true,
-      hmr: {
-        // Use websocket for mobile hot reloading
-        protocol: 'ws',
-        // Make sure it's available on the network
-        host: '0.0.0.0',
-        // Use a specific port for hmr
-        port: 5183,
-      },
-    }
   },
   typescript: {
     typeCheck: true,
@@ -69,7 +50,4 @@ export default defineNuxtConfig({
       }
     }
   },
-  // For Tauri
-  ssr: false,
-
 })
