08bef053e2f6ecbc742cd774d164c913b40cf413
Klay Lee <ads1029384756@gmail.com>
2026-04-13 01:12:43 +0800

    fix: autocomplete issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         {
           name: 'viewport',
           content:
-          'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0'
+          'width=device-width,initial-scale=1.0'
         }
       ],
     }
